687ff6982d95d60751024672d5bff492642b1075
Mark Peters <mapeters101@earthlink.net>
2024-12-05 15:31:05 -0500

    Complete lesson094.

--- lesson094.cpp
+++ lesson094.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-/**/
+/*
 int main() {
 	int Array[5]{ 1, 2, 3, 4, 5 };
 
@@ -19,13 +19,32 @@ int main() {
 	int ArrayS[5]{ 1, 2, 3, 4, 5 };
 	int* ArraySptr{ ArrayS };
 	int* ArraySptr2{ ArrayS + 4 };
+	cout << "------------------------------------\n\n";
 
 	auto Difference{ ArraySptr2 - ArraySptr };
 	cout << Difference << endl;
 
+	int NewArray[5]{ 3, 9, 5, 2, 1 };
+
+	int* NewArrayptr{ NewArray };
+
+	for (int i{ 0 }; i < 5; i++) {
+		cout << *NewArrayptr++ << endl;
+	}
+	cout << "\n\n";
+
+	double NewArray2[5]{ 3.0, 9.6, 5.9, 2.2, 11.6 };
+
+	double* NewArrayptr2{ NewArray2 };
+
+	for (int i{ 0 }; i < 5; i++) {
+		cout << *NewArrayptr2++ << endl;
+	}
+	cout << "\n\n";
+
 
 	return 0;
 }
-
+*/
 
 
