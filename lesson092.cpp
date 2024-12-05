012e34776943ee157370d87f2d1dd6608fba68a6
Mark Peters <mapeters101@earthlink.net>
2024-12-05 13:44:54 -0500

    Work on lesson092.

---
+++ lesson092.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+int main() {
+	int* Array[3];
+
+	int Num1{ 15 }, Num2{ 25 }, Num3{ 34 };
+
+	Array[0] = &Num1;
+	Array[1] = &Num2;
+	Array[2] = &Num3;
+
+	// Address
+	cout << "Addresses: " << endl;
+	cout << Array[0] << endl;
+	cout << Array[1] << endl;
+	cout << Array[2] << endl << endl;
+
+	// Values
+	cout << "Values: " << endl;
+	cout << *Array[0] << endl;
+	cout << *Array[1] << endl;
+	cout << *Array[2] << endl << endl;
+	cout << "-------------------------------------\n\n";
+
+	int Array1[5]{ 1, 2, 3, 4, 5 };
+	int* Array1ptr{ Array1 };
+
+	cout << "Addresses: " << endl;
+	cout << Array1 << endl;
+	cout << Array1ptr << endl;
+	cout << Array1ptr + 1 << endl;
+	cout << Array1ptr + 2 << endl;
+	cout << Array1ptr + 3 << endl;
+	cout << Array1ptr + 4 << endl;
+
+	cout << "\nValues: " << endl;
+	cout << Array1[0] << endl;
+	cout << *Array1ptr << endl;
+	cout << *(Array1ptr + 1) << endl;
+	cout << *(Array1ptr + 2) << endl;
+	cout << *(Array1ptr + 3) << endl;
+	cout << *(Array1ptr + 4) << endl;
+
+
+	return 0;
+}
+*/
+
