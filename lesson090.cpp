da5185a448904ed2af3e6ff06971345459e829ca
Mark Peters <mapeters101@earthlink.net>
2024-12-05 12:48:43 -0500

    Work on lesson090.

--- lesson090.cpp
+++ lesson090.cpp
@@ -1,4 +1,20 @@
+#include <iostream>
+
+using namespace std;
 
 
+/*
+int main() {
+	int Num{ 13 };
+	cout << "Value of Num: " << Num << endl;
+	cout << "Address of Num: " << &Num << endl;
+
+	int* Numptr{ &Num };
+	cout << "Value of Numptr: " << Numptr << endl;
+	cout << "Address of Numptr: " << &Numptr << endl;
+
+	return 0;
+}
+*/
 
 
