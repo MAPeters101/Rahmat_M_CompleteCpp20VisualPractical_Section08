7b240353bc27811ef06c21f8d648356e75c0ad3e
Mark Peters <mapeters101@earthlink.net>
2024-12-05 12:57:16 -0500

    Work on lesson091.

---
+++ lesson091.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+int main() {
+	int Num{ 13 };
+	cout << "Value of Num: " << Num << endl;
+	cout << "Address of Num: " << &Num << endl;
+
+	int* Numptr{ &Num };
+	cout << "Value of Numptr: " << Numptr << endl;
+	cout << "Address of Numptr: " << &Numptr << endl;
+
+	cout << "Dereferencing Numptr: " << *Numptr << endl;
+
+	*Numptr = 19;
+	cout << "Value of Num: " << Num << endl;
+
+	int Num2{ 31 };
+	Numptr = &Num2;
+	cout << "Value of Numptr: " << Numptr << endl;
+	cout << "Address of Numptr: " << &Numptr << endl;
+
+	cout << "Dereferencing Numptr: " << *Numptr << endl;
+
+
+	return 0;
+}
+*/
+
