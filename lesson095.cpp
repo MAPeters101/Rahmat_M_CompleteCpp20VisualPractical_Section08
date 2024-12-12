8c2adca6e07063aa446a3f753c57c9734080f3dc
Mark Peters <mapeters101@earthlink.net>
2024-12-12 12:04:05 -0500

    Complete lesson095.

---
+++ lesson095.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+int main() {
+	int* Numberptr{ new int{26} };
+
+	cout << "Address of Numberptr: " << Numberptr << endl;
+	cout << "Value of Numberptr: " << *Numberptr << endl;
+
+	int* DynamicArray{ new int[10] {1, 2, 3, 4, 5, 6, 7} };
+	cout << "Address of DynamicArray: " << DynamicArray << endl;
+	cout << "Value of DynamicArray 1: " << DynamicArray[0] << endl;
+	cout << "Value of DynamicArray 2: " << DynamicArray[1] << endl;
+	cout << "Value of DynamicArray 3: " << DynamicArray[2] << endl;
+	DynamicArray[2] = 30;
+	cout << "Value of DynamicArray 3: " << DynamicArray[2] << endl;
+
+	delete Numberptr;
+	delete[] DynamicArray;
+
+	//cout << "Value of Numberptr: " << *Numberptr << endl;
+	//cout << "Print this line??" << endl;
+
+	if (Numberptr != nullptr)
+		cout << "Print line 1..." << endl;
+
+
+	Numberptr = nullptr;
+	DynamicArray = nullptr;
+
+	if (Numberptr != nullptr)
+		cout << "Print line 2..." << endl;
+
+	//cout << "Value of Numberptr: " << *Numberptr << endl;
+	cout << "Print this line??" << endl;
+
+
+	return 0;
+}
+
+*/
