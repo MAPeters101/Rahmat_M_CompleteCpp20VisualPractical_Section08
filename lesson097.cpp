347a05226ecb58c5480dd4e94d437059f83d9213
Mark Peters <mapeters101@earthlink.net>
2024-12-12 13:01:17 -0500

    Complete lesson097.

--- lesson097.cpp
+++ lesson097.cpp
@@ -2,33 +2,33 @@
 
 using namespace std;
 
-
+/*
 int main() {
-	/*
-	int* Number{ new int{26} };
-	int* DynamicArray{ new int[10] {1,2,3,4,5,6} };
+	
+	//int* Number{ new int{26} };
+	//int* DynamicArray{ new int[10] {1,2,3,4,5,6} };
 
-	cout << "Address of Number: " << Number << endl;
-	cout << "Value of Number: " << *Number << endl;
-
-	cout << "Address of Array: " << DynamicArray << endl;
-	cout << "Value of Array: " << DynamicArray[0] << endl;
-
-	Number = new int{ 29 };  // Creates a memory leak without deleting previous int=26
-	cout << "Address of Number: " << Number << endl;
-	cout << "Value of Number: " << *Number << endl;
-
-	delete Number;
-	delete[] DynamicArray;
-	cout << "Address of Number: " << Number << endl;
-	cout << "Address of Array: " << DynamicArray << endl;
-
+	//cout << "Address of Number: " << Number << endl;
 	//cout << "Value of Number: " << *Number << endl;
+
+	//cout << "Address of Array: " << DynamicArray << endl;
 	//cout << "Value of Array: " << DynamicArray[0] << endl;
 
-	delete Number;
-	delete[] DynamicArray;
-*/
+	//Number = new int{ 29 };  // Creates a memory leak without deleting previous int=26
+	//cout << "Address of Number: " << Number << endl;
+	//cout << "Value of Number: " << *Number << endl;
+
+	//delete Number;
+	//delete[] DynamicArray;
+	//cout << "Address of Number: " << Number << endl;
+	//cout << "Address of Array: " << DynamicArray << endl;
+
+	////cout << "Value of Number: " << *Number << endl;
+	////cout << "Value of Array: " << DynamicArray[0] << endl;
+
+	//delete Number;
+	//delete[] DynamicArray;
+
 
 	// Correct ways.
 	double* Number1{ new double{26} };
@@ -40,12 +40,31 @@ int main() {
 	cout << "Address of DynamicArray1: " << DynamicArray1 << endl;
 	cout << "Value of DynamicArray1: " << DynamicArray1[0] << endl;
 
+	delete Number1;
+	Number1 = nullptr;
+	delete[] DynamicArray1;
+	DynamicArray1 = nullptr;
 
 
+	if (Number1) {
+		// If Number1 is valid, it's ok to use.
+	}
+	if (Number1 != nullptr) {
+		// If Number1 is valid, it's ok to use.
+	}
 
 
+	double Number2{26};
+	int Array1[10] {1,2,3,4,5,6};
 
+	cout << "\n\nAddress of Number2: " << &Number2 << endl;
+	cout << "Value of Number2: " << Number2 << endl;
+
+	cout << "Address of Array1: " << Array1 << endl;
+	cout << "Value of Array1: " << Array1[0] << endl;
 
 
 	return 0;
 }
+
+*/
